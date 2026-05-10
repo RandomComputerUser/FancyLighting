dac59dee9ba0b47ca2f5f6a7db22576d87b8fe44
Rex Berry <59031902+RexBerry@users.noreply.github.com>
2026-05-09 19:21:09 -0500

    More visual tweaks

--- ColorProfiles/SkyColor/SkyColorsLow.cs
+++ ColorProfiles/SkyColor/SkyColorsLow.cs
@@ -14,7 +14,7 @@ public sealed class SkyColorsLow : LoadableColorProfileBase, ISimpleColorProfile
 
         var nightColor = new Vector3(0.04f, 0.04f, 0.055f);
         var nightColor3 = new Vector3(0.05f, 0.05f, 0.09f);
-        var nightColor2 = new Vector3(0.08f, 0.07f, 0.19f);
+        var nightColor2 = new Vector3(0.08f, 0.07f, 0.18f);
         var nightColor1 = new Vector3(0.37f, 0.28f, 0.28f);
         var sunriseSunsetColor = new Vector3(0.76f, 0.55f, 0.33f);
         var dayColor1 = new Vector3(0.84f, 0.72f, 0.47f);
